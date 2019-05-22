22220dbdf7271fb772a9876cdb78258c7f6bebd0
yottanami <yottanami@gmail.com>
2019-05-22 17:59:43 +0430

    Add devise install in install generator

--- lib/generators/rails_graphql/install_generator.rb
+++ lib/generators/rails_graphql/install_generator.rb
@@ -3,8 +3,54 @@ module RailsGraphql
   class  InstallGenerator < Rails::Generators::Base
     #  source_root File.expand_path('templates', __dir__)
 
+
     def install_graphql
+      if behavior == :invoke
+        generate "graphql:install"
+        install_authentication
+      else
+        # TODO: Add destroy feature
+        # Rails::Generators.invoke 'devise:install', :behavior => :revoke
+        puts "Destroy does not support yet, you can destroy generators manually."
+      end
+    end
+
+    private
+    def install_authentication
+      puts self.behavior
+      puts "Select your authentication method"
+      puts "1: No Authentication"
+      puts "2: Install Devise"
+      puts "3: Install OTP base"
+
+      auth_type = ''
+      loop do
+        auth_type = STDIN.gets.chomp
+        break if [1,2,3].include? auth_type.to_i
+      end
+
+      if auth_type == '2'
+        install_devise
+        install_devise_user
+
+      elsif auth_type == 3
+        install_otp
+      end
+    end
+
+    private
+
+    def install_devise
+      gem 'devise'
+      generate "devise:instal"
+      generate("devise", "user")
+      # config/environments/development.rb
+      # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    end
+
+    def install_otp
 
     end
+
   end
 end
